97e0a3976ec22bd7fc10f91785a3a2629c124c5f
y0urak <110369347+y0urak@users.noreply.github.com>
2026-08-09 01:38:25 +0900

    Add files via upload

---
+++ kimochi.info_redirector.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         kimochi.info_redirector
+// @namespace    http://tampermonkey.net/
+// @version      2026-08-08
+// @description  try to take over the world!
+// @author       y0urak
+// @match        https://kimochi.info/download/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const match = location.href.match(/\/download\/(?!file\/)([^\/]+)/);
+
+    if (match?.[1]) {
+        location.replace(`https://kimochi.info/download/file/?dl=${match[1]}`);
+    }
+})();
